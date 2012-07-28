19a650e394a1d113b9e71d3daec1a2e119336538
Tony Marklove <tony@new-bamboo.co.uk>
2012-07-28 14:04:24 +0100

    Setup bacic rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitbitmap::Application.config.secret_token = '22ee01fee5cb79d8d427baeb65df66ed7c0e6ebb8a62cf752ef93302c9c96ca8995f5aa81df04044613a969ef7a66079d16d2aea55e3bb5799a0c93127824cc4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Twitbitmap::Application.config.session_store :cookie_store, key: '_twitbitmap_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Twitbitmap::Application.config.session_store :active_record_store
